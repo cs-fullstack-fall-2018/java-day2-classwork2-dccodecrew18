7f31dde3e91f48fc752a7907c1286d46b8c801c6
Darius Carter <dcarterbusinessemail@gmail.com>
2018-07-23 15:20:42 -0500

    Ex 12

---
+++ src/com/company/Excercise12.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Excercise12{
+
+    public static void main() {
+        // write your code here
+    }
+
+    public static void countFunt(){
+        int number = 0;
+        while (number< 6);
+        System.out.print(++number);
+        countFunt();
+    }
+
+}
+
